cb147fdbdef3d5e9083bee6dc5bf0655128479c0
AbdulKareemios <a.kareem.tn@gmail.com>
2025-10-07 13:19:06 +0500

    Update version-info.js

--- api/src/version-info.js
+++ api/src/version-info.js
@@ -1,5 +1,7 @@
-export default {
-  version: "1.0.0",
-  build: "render-deploy",
-  date: new Date().toISOString()
-};
+export function getVersion() {
+  return {
+    version: "1.0.0",
+    build: "render-deploy",
+    date: new Date().toISOString()
+  };
+}
